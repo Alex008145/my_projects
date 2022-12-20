c701967316eeed4672f5385b900bb78b863e2463
John Sato <alex008145@gmail.com>
2022-12-20 11:51:56 +0300

    20.12

--- JavaScript projects/1. Guess My Number/script.js
+++ JavaScript projects/1. Guess My Number/script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 /*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'Correct Number!';
@@ -15,45 +15,45 @@ let score = 20;
 let highscore = 0;
 
 const displayMessage = function (message) {
-  document.querySelector('.message').textContent = message;
+  document.querySelector(".message").textContent = message;
 };
 
 // Check button
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
 
   // When there is no input
   if (!guess) {
     // document.querySelector('.message').textContent = 'No number!';
-    displayMessage('No number!');
+    displayMessage("No number!");
 
     // When player wins
   } else if (guess === secretNumber) {
     // document.querySelector('.message').textContent = 'Correct Number!';
-    displayMessage('Correct Number!');
+    displayMessage("Correct Number!");
 
-    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector(".number").textContent = secretNumber;
 
-    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector("body").style.backgroundColor = "#60b347";
 
-    document.querySelector('.number').style.width = '30rem';
+    document.querySelector(".number").style.width = "30rem";
 
     if (score > highscore) {
       highscore = score;
-      document.querySelector('.highscore').textContent = highscore;
+      document.querySelector(".highscore").textContent = highscore;
     }
   } else if (guess !== secretNumber) {
     if (score > 1) {
       //   document.querySelector('.message').textContent =
       //     guess > secretNumber ? 'Too high!' : 'Too low!';
-      displayMessage(guess > secretNumber ? 'Too high!' : 'Too low!');
+      displayMessage(guess > secretNumber ? "Too high!" : "Too low!");
       score--;
-      document.querySelector('.score').textContent = score;
+      document.querySelector(".score").textContent = score;
     } else {
       //   document.querySelector('.message').textContent = 'Game Over';
-      displayMessage('Game Over');
-      document.querySelector('.score').textContent = 0;
+      displayMessage("Game Over");
+      document.querySelector(".score").textContent = 0;
     }
   }
 });
@@ -82,15 +82,15 @@ document.querySelector('.check').addEventListener('click', function () {
 // });
 
 // Again button
-document.querySelector('.again').addEventListener('click', function () {
+document.querySelector(".again").addEventListener("click", function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
 
   //   document.querySelector('.message').textContent = 'Start guessing...';
-  displayMessage('Start quessing...');
-  document.querySelector('.score').textContent = score;
-  document.querySelector('.guess').value = '';
-  document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '15rem';
-  document.querySelector('.number').textContent = '?';
+  displayMessage("Start quessing...");
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".guess").value = "";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+  document.querySelector(".number").textContent = "?";
 });
--- черновик/skript.js
+++ черновик/skript.js
@@ -1,2 +1,5 @@
-let planetEarth;
-let userName;
+let a = 2;
+
+let x = 1 + (a *= 2);
+
+console.log(a, x);
