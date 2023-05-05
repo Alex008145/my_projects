07b794b94d424a9cf55dc2ee52c417a013ce8d6d
John Sato <alex008145@gmail.com>
2023-05-05 11:34:11 +0300

    05.05

--- typescript-react-project/src/App.tsx
+++ typescript-react-project/src/App.tsx
@@ -1,28 +1,41 @@
-import React, { useEffect, useState } from "react";
+import { useState } from "react";
+import { CreateProduct } from "./components/CreateProduct";
+import { ErrorMessage } from "./components/ErrorMessage";
+import { Loader } from "./components/Loader";
+import { Modal } from "./components/Modal";
 import { Product } from "./components/Product";
-import { products } from "./data/products";
-import axios from "axios";
+import { useProducts } from "./hooks/products";
 import { IProduct } from "./models";
 
 function App() {
-  const [products, setProducts] = useState<IProduct[]>([]);
+  const { loading, error, products, addProduct } = useProducts();
+  const [modal, setModal] = useState(false);
 
-  async function fetchProducts() {
-    const response = await axios.get<IProduct[]>(
-      "https://fakestoreapi.com/products"
-    );
-    console.log(response);
-  }
-
-  useEffect(() => {
-    fetchProducts();
-  }, []);
+  const createHandler = (product: IProduct) => {
+    setModal(false);
+    addProduct(product);
+  };
 
   return (
     <div className="container mx-auto max-w-2xl pt-5">
+      {loading && <Loader />}
+      {error && <ErrorMessage error={error} />}
       {products.map((product) => (
         <Product product={product} key={product.id} />
       ))}
+
+      {modal && (
+        <Modal title="Create new product" onClose={() => setModal(false)}>
+          <CreateProduct onCreate={createHandler} />
+        </Modal>
+      )}
+
+      <button
+        className="fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 py-2"
+        onClick={() => setModal(true)}
+      >
+        +
+      </button>
     </div>
   );
 }
