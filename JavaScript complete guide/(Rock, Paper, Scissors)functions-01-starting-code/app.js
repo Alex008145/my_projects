1c268e0b521f7ce65360201343bd85e9ac8fdfe8
John Sato <alex008145@gmail.com>
2023-08-01 19:40:31 +0300

    01.08

--- JavaScript complete guide/(Rock, Paper, Scissors)functions-01-starting-code/app.js
+++ JavaScript complete guide/(Rock, Paper, Scissors)functions-01-starting-code/app.js
@@ -33,8 +33,6 @@ const getComputerChoice = () => {
   }
 };
 
-const add = (a, b) => a + b;
-
 const getWinner = (cChoice, pChoice = DEFAULT_USER_CHOICE) =>
   cChoice === pChoice
     ? RESULT_DRAW
@@ -62,7 +60,7 @@ startGameBtn.addEventListener("click", () => {
   }
   gameIsRunning = true;
   console.log("Game is starting...");
-  const playerChoice = getPlayerChoice(); // might be undefined
+  const playerChoice = getPlayerChoice();
   const computerChoice = getComputerChoice();
   let winner;
   if (playerChoice) {
@@ -72,7 +70,7 @@ startGameBtn.addEventListener("click", () => {
   }
 
   let message = `You picked ${
-    playerChoice ? playerChoice : DEFAULT_USER_CHOICE
+    playerChoice || DEFAULT_USER_CHOICE
   }, computer picked ${computerChoice}, therefore you `;
   if (winner === RESULT_DRAW) {
     message = message + "had a draw.";
@@ -84,3 +82,65 @@ startGameBtn.addEventListener("click", () => {
   alert(message);
   gameIsRunning = false;
 });
+
+// not related to game
+
+const combine = (resultHandler, operation, ...numbers) => {
+  const validateNumber = (number) => {
+    return isNaN(number) ? 0 : number;
+  };
+
+  let sum = 0;
+  for (const num of numbers) {
+    if (operation === "ADD") {
+      sum += validateNumber(num);
+    } else {
+      sum -= validateNumber(num);
+    }
+  }
+  resultHandler(sum);
+};
+
+// const subtractUp = function (resultHandler, ...numbers) {
+//   let sum = 0;
+//   for (const num of numbers) {
+//     // don't use that
+//     sum -= num;
+//   }
+//   resultHandler(sum, "The result after adding all numbers is");
+// };
+
+const showResult = (messageText, result) => {
+  alert(messageText + " " + result);
+};
+
+combine(
+  showResult.bind(this, "The result after adding all numbers is: "),
+  "ADD",
+  1,
+  5,
+  "dfadf",
+  -3,
+  6,
+  10
+);
+combine(
+  showResult.bind(this, "The result after adding all numbers is: "),
+  "ADD",
+  1,
+  5,
+  10,
+  -3,
+  6,
+  10,
+  25,
+  88
+);
+combine(
+  showResult.bind(this, "The result after subtracting all numbers is: "),
+  "SUBTRACT",
+  1,
+  10,
+  15,
+  20
+);
