cd79f923d7c396207f7a9e226b35eb2421dafeab
John Sato <alex008145@gmail.com>
2023-08-17 01:56:03 +0300

    17.08

---
+++ JavaScript complete guide/prototypes-01-starting-setup/app.js
@@ -0,0 +1,118 @@
+class AgedPerson {
+  constructor() {
+    console.log(this.name);
+  }
+
+  printAge() {
+    console.log(this.age);
+  }
+}
+
+class Person {
+  name = 'Max';
+
+  constructor() {
+    // super();
+    this.age = 30;
+  }
+
+  // greet = () => {
+  //   console.log(
+  //     'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
+  //   );
+  // };
+
+  greet() {
+    console.log(
+      'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
+    );
+  }
+}
+
+// function Person() {
+//   this.age = 30;
+//   this.name = 'Max';
+// }
+
+// Person.prototype.greet = function () {
+//   console.log(
+//     'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
+//   );
+// };
+
+// Person.describe = function () {
+//   console.log('Creating persons...');
+// };
+
+// Person.prototype.printAge = {
+//   printAge() {
+//     console.log(this.age);
+//   },
+// };
+
+// Person.prototype.printAge = function () {
+//   console.log(this.age);
+// };
+
+// console.dir(Person);
+
+// const p = new Person();
+// p.greet();
+// p.printAge();
+// console.log(p);
+// console.log(p.toString());
+// const p2 = p.__proto__.constructor();
+// console.log(p2);
+// console.dir(Object.prototype);
+
+// const p = new Person();
+// const p2 = new Person();
+// p.greet();
+// console.log(p);
+
+// const button = document.getElementById('btn');
+// button.addEventListener('click', p.greet.bind(p));
+
+const course = {
+  title: 'JavaScript - The Complete Guide',
+  rating: 5,
+};
+
+// console.log(Object.getPrototypeOf(course));
+Object.setPrototypeOf(course, {
+  // ...Object.getPrototypeOf(course),
+  printRating: function () {
+    console.log(`${this.rating}/5`);
+  },
+});
+
+const student = Object.create(
+  {
+    printProgress: function () {
+      console.log(this.progress);
+    },
+  },
+  {
+    name: {
+      configurable: true,
+      enumerable: true,
+      value: 'Max',
+      writable: true,
+    },
+  }
+);
+
+// student.name = 'Max';
+
+Object.defineProperty(student, 'progress', {
+  configurable: true,
+  enumerable: true,
+  value: 0.8,
+  writable: false,
+});
+
+student.printProgress();
+
+console.log(student);
+
+course.printRating();
