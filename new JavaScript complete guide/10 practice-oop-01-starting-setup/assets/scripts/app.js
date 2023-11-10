2c2ddda16e9d93bfe8d1fdf503e4297de6ecf239
John Sato <alex008145@gmail.com>
2023-11-10 21:38:42 +0300

    10.11

---
+++ new JavaScript complete guide/10 practice-oop-01-starting-setup/assets/scripts/app.js
@@ -0,0 +1,157 @@
+class DOMHelper {
+  static clearEventListeners(element) {
+    const clonedElement = element.cloneNode(true);
+    element.replaceWith(clonedElement);
+    return clonedElement;
+  }
+
+  static moveElement(elementId, newDestanationSelector) {
+    const element = document.getElementById(elementId);
+    const destinationElement = document.querySelector(newDestanationSelector);
+    destinationElement.append(element);
+  }
+}
+
+class Component {
+  constructor(hostElementId, insertBefore = false) {
+    if (hostElementId) {
+      this.hostElement = document.getElementById(hostElementId);
+    } else {
+      this.hostElement = document.body;
+    }
+    this.insertBefore = insertBefore;
+  }
+
+  detach() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  attach() {
+    this.hostElement.insertAdjacentElement(
+      this.insertBefore ? 'beforebegin' : 'beforeend',
+      this.element
+    );
+  }
+}
+
+// modal window
+class ToolTip extends Component {
+  constructor(closeNotifierFunction) {
+    super();
+    this.closeNotifier = closeNotifierFunction;
+    this.create();
+  }
+  closeTooltip = () => {
+    this.detach();
+    this.closeNotifier();
+  };
+
+  create() {
+    const tooltipElement = document.createElement('div');
+    tooltipElement.className = 'card';
+    tooltipElement.textContent = 'Some text';
+    tooltipElement.addEventListener('click', this.closeTooltip);
+    this.element = tooltipElement;
+  }
+}
+
+// class for individual project
+class ProjectItem {
+  hasActiveTooltip = false;
+
+  constructor(id, updateProjectListsFunction, type) {
+    this.id = id;
+    this.updateProjectListsHandler = updateProjectListsFunction;
+    this.connectMoreInfoButton();
+    this.connectSwitchButton(type);
+  }
+
+  showMoreInfoHandler() {
+    if (this.hasActiveTooltip) {
+      return;
+    }
+    const tooltip = new ToolTip(() => {
+      this.hasActiveTooltip = false;
+    });
+    tooltip.attach();
+    this.hasActiveTooltip = true;
+  }
+
+  connectMoreInfoButton() {
+    const projectItemElement = document.getElementById(this.id);
+    const moreInfoBtn = projectItemElement.querySelector(
+      'button:first-of-type'
+    );
+    moreInfoBtn.addEventListener('click', this.showMoreInfoHandler);
+  }
+
+  connectSwitchButton(type) {
+    const projectItemElement = document.getElementById(this.id);
+    let switchBtn = projectItemElement.querySelector('button:last-of-type');
+    switchBtn = DOMHelper.clearEventListeners(switchBtn);
+    switchBtn.textContent = type === 'active' ? 'Finish' : 'Activate';
+    switchBtn.addEventListener(
+      'click',
+      this.updateProjectListsHandler.bind(null, this.id)
+    );
+  }
+
+  update(updateProjectListsFn, type) {
+    this.updateProjectListsHandler = updateProjectListsFn;
+    this.connectSwitchButton(type);
+  }
+}
+
+// active or finished block
+class ProjectList {
+  projects = [];
+
+  constructor(type) {
+    this.type = type;
+
+    const prjItems = document.querySelectorAll(`#${type}-projects li`);
+    for (const prjItem of prjItems) {
+      this.projects.push(
+        new ProjectItem(prjItem.id, this.switchProject.bind(this), this.type)
+      );
+    }
+    console.log(this.projects);
+  }
+
+  setSwitchHandlerFunction(switchHandlerFunction) {
+    this.switchHandler = switchHandlerFunction;
+  }
+
+  // add project to list
+  addProject(project) {
+    this.projects.push(project);
+    DOMHelper.moveElement(project.id, `#${this.type}-projects ul`);
+    project.update(this.switchProject.bind(this), this.type);
+  }
+
+  // move project to another list
+  switchProject(projectId) {
+    // const projectIndex = this.projects.findIndex((p) => p.id === projectId);
+    // this.project.splice(projectIndex, 1);
+    this.switchHandler(this.projects.find((p) => p.id === projectId));
+    this.projects = this.projects.filter((p) => p.id !== projectId);
+  }
+}
+
+// main class
+class App {
+  static init() {
+    const activeProjectsList = new ProjectList('active');
+    const finishedProjectsList = new ProjectList('finished');
+    activeProjectsList.setSwitchHandlerFunction(
+      finishedProjectsList.addProject.bind(finishedProjectsList)
+    );
+    finishedProjectsList.setSwitchHandlerFunction(
+      activeProjectsList.addProject.bind(activeProjectsList)
+    );
+  }
+}
+
+App.init();
--- new JavaScript complete guide/9 prototypes-01-starting-setup/app.js
+++ new JavaScript complete guide/9 prototypes-01-starting-setup/app.js
@@ -1,36 +1,121 @@
-// class Person {
-//   name = 'Max';
+class AgedPerson {
+  constructor() {
+    console.log(this.name);
+  }
 
-//   constructor() {
-//     this.age = 30;
-//   }
+  printAge() {
+    console.log(this.age);
+  }
+}
 
-//   greet() {
-//     console.log(
-//       'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
-//     );
-//   }
-// }
+class Person {
+  name = 'Max';
 
-function Person() {
-  this.age = 30;
-  this.name = 'Max';
-  this.greet = function () {
+  constructor() {
+    // super();
+    this.age = 30;
+    // this.greet = function() { ... }
+  }
+
+  // greet = () => {
+  //   console.log(
+  //     'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
+  //   );
+  // };
+
+  greet() {
     console.log(
       'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
     );
-  };
+  }
 }
 
-Person.prototype = {
-  printAge() {
-    console.log(this.age);
-  },
+// function Person() {
+//   this.age = 30;
+//   this.name = 'Max';
+//   // this.greet = function() { ... }
+// }
+
+// Person.prototype.greet = function () {
+//   console.log(
+//     'Hi, I am ' + this.name + ' an d I am ' + this.age + ' years old.'
+//   );
+// };
+
+// Person.describe = function () {
+//   console.log('Creating persons...');
+// };
+
+// Person.prototype = {
+//   printAge() {
+//     console.log(this.age);
+//   },
+// };
+
+// Person.prototype.printAge = function () {
+//   console.log(this.age);
+// };
+
+// console.dir(Person);
+// console.dir(Object);
+
+// const p = new Person();
+// p.greet();
+// p.printAge();
+// console.log(p.length);
+// console.log(p.toString());
+// const p2 = new Person();
+// console.log(p2);
+// console.dir(Object.prototype);
+
+// const p = new Person();
+// const p2 = new Person();
+// p.greet();
+// console.log(p);
+
+// const button = document.getElementById('btn');
+// button.addEventListener('click', p.greet.bind(p));
+
+// new Object()
+const course = {
+  title: 'JavaScript- The Complete Guide',
+  rating: 5,
 };
 
-console.dir(Person);
+// console.log(Object.getPrototypeOf(course));
+console.log(course.__proto__);
+Object.setPrototypeOf(course, {
+  ...Object.getPrototypeOf(course),
+  printRating: function () {
+    console.log(`${this.rating}/5`);
+  },
+});
 
-const p = new Person();
-p.greet();
-p.printAge();
-console.log(p.__proto__);
+const student = Object.create(
+  {
+    printProgress: function () {
+      console.log(this.progress);
+    },
+  },
+  {
+    name: {
+      configurable: true,
+      enumerable: true,
+      value: 'Max',
+      writable: true,
+    },
+  }
+);
+
+student.name = 'Max';
+
+// Object.defineProperty(student, 'progress', {
+//   configurable: true,
+//   enumerable: true,
+//   value: 0.8,
+//   writable: false,
+// });
+
+console.log(student);
+
+course.printRating();
