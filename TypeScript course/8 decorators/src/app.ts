2bde053f1c7b2632c8ce29cf1476d76885bef363
John Sato <alex008145@gmail.com>
2023-12-12 00:10:21 +0300

    12.12

--- TypeScript course/8 decorators/src/app.ts
+++ TypeScript course/8 decorators/src/app.ts
@@ -134,12 +134,40 @@ interface ValidatorConfig {
 const registeredValidators: ValidatorConfig = {};
 
 function Required(target: any, propName: string) {
-  registeredValidators[target.constructor.name];
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: ['required'],
+  };
 }
 
-function PositiveNumber() {}
+function PositiveNumber(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: ['positive'],
+  };
+}
 
-function validate(obj: object) {}
+function validate(obj: any) {
+  const objValidatorConfig = registeredValidators[obj.constructor.name];
+  if (!objValidatorConfig) {
+    return true;
+  }
+  let isValid = true;
+  for (const prop in objValidatorConfig) {
+    console.log(prop);
+    for (const validator of objValidatorConfig[prop]) {
+      switch (validator) {
+        case 'required':
+          isValid = isValid && !!obj[prop];
+          break;
+        case 'positive':
+          isValid = isValid && obj[prop] > 0;
+          break;
+      }
+    }
+  }
+  return isValid;
+}
 
 class Course {
   @Required
