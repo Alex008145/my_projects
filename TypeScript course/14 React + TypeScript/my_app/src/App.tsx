44587c06f3b28105ca544d74b4fda4c3bf86ed64
John Sato <alex008145@gmail.com>
2023-12-15 19:04:27 +0300

    15.12

--- TypeScript course/14 React + TypeScript/my_app/src/App.tsx
+++ TypeScript course/14 React + TypeScript/my_app/src/App.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Todo } from './todo.model';
+import { Route } from 'react-router-dom';
+
+import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const todoAddHandler = (text: string) => {
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text },
+    ]);
+  };
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
+
   return (
     <div className="App">
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
