abf4d85315bb8db583d1a8af14eac0bc176ab6c1
John Sato <alex008145@gmail.com>
2024-01-04 20:45:41 +0300

    04.01

--- React-the-complete-guide/4-starting-project/src/App.js
+++ React-the-complete-guide/4-starting-project/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import { useState } from 'react';
 
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList(() => {
+      return [...prevUserList];
+    });
+  };
+
   return (
     <div>
-
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
