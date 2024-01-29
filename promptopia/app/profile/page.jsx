175f47e7932b9fc975fe0554d9dee82026708f38
John Sato <alex008145@gmail.com>
2024-01-29 15:12:29 +0300

    29.01

--- promptopia/app/profile/page.jsx
+++ promptopia/app/profile/page.jsx
@@ -6,8 +6,35 @@ import { useRouter } from 'next/navigation';
 
 import Profile from '@components/profile';
 
-const Profile = () => {
-  return <Profile name="My Profile" desc="Welcome to your profile page" data={[]} handleEdit={} handleDelete={}/>;
+const MyProfile = () => {
+  const { data: session } = useSession();
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+
+      setPosts(data);
+    };
+
+    if (session?.user.id) fetchPosts();
+  }, []);
+
+  const handleEdit = () => {};
+
+  const handleDelete = async () => {};
+
+  return (
+    <Profile
+      name="My"
+      desc="Welcome to your profile page"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
 };
 
-export default Profile;
+export default MyProfile;
